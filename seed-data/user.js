3d31a27350acaed4d6a75415901f8e5ccc61181c
i-am-lp <lp.phillips95@gmail.com>
2025-01-24 00:05:30 -0500

    added user emails

--- seed-data/user.js
+++ seed-data/user.js
@@ -1,22 +1,29 @@
-export default [
-    {
-      id: 1,
-      name: "Lauren Phillips",
-    },
-    {
+const users = [
+  {
+    id: 1,
+    name: "Lauren Phillips",
+    email: "lauren.phillips@example.com",
+  },
+  {
     id: 2,
     name: "Dana Osborn",
-    },
-    {
+    email: "dana.osborn@example.com",
+  },
+  {
     id: 3,
     name: "Geena Massara",
-    },
+    email: "geena.massara@example.com",
+  },
   {
     id: 4,
     name: "Andrea Larsen",
-    },
+    email: "andrea.larsen@example.com",
+  },
   {
     id: 5,
     name: "Jordan Phillips",
-    }
-]
+    email: "jordan.phillips@example.com",
+  },
+];
+
+export default users;
--- seeds/newsletter-response.js
+++ seeds/newsletter-response.js
@@ -1,17 +1,14 @@
-// import seed data files, arrays of objects
 import newsletter from "../seed-data/newsletter.js";
 import responses from "../seed-data/responses.js";
 import users from "../seed-data/user.js"
 
 export async function seed(knex) {
-
   await knex.raw("SET FOREIGN_KEY_CHECKS = 0;");
-
   await knex("responses").truncate();
   await knex("newsletter").truncate();
   await knex("user").truncate();
+  await knex.raw("SET FOREIGN_KEY_CHECKS = 1;"); 
 
-  await knex.raw("SET FOREIGN_KEY_CHECKS = 1;");
 
   await knex("user").insert(users);
   await knex("newsletter").insert(newsletter);
